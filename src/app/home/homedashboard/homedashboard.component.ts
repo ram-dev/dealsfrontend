eb941156b71d2ec0ddcd153a030620fd11ef4ecc
chintalaravi teja <chintalaravi.teja@kohls.com>
2017-11-08 17:58:25 +0530

    slider path issue

--- src/app/home/homedashboard/homedashboard.component.ts
+++ src/app/home/homedashboard/homedashboard.component.ts
@@ -10,17 +10,17 @@ export class HomeDashboardComponent {
 
   constructor() {
     this.sliders.push({
-            imagePath: '../assets/images/camera1.jpg',
+            imagePath: 'assets/images/camera1.jpg',
             label: 'First slide label',
-            text: 'Nulla vitae elit libero, a pharetra augue mollis interdum.'
+            text: 'First'
         }, {
-            imagePath: '../assets/images/camera2.jpg',
+            imagePath: 'assets/images/camera2.jpg',
             label: 'Second slide label',
-            text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+            text: 'Second'
         }, {
-            imagePath: '../assets/images/camera3.jpg',
+            imagePath: 'assets/images/camera3.jpg',
             label: 'Third slide label',
-            text: 'Praesent commodo cursus magna, vel scelerisque nisl consectetur.'
+            text: 'Third'
         });
   }
 
